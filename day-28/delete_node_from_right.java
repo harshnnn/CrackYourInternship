70727c8a9321e052ab9a9ad25bd594e264a2ee0c
Harsh Ranjan Pathak <72537619+harshnnn@users.noreply.github.com>
2024-08-12 22:51:11 +0530

    day-29

--- day-28/delete_node_from_right.java
+++ day-28/delete_node_from_right.java
@@ -1,5 +1,41 @@
-package day-28;
-
-public class delete_node_from_right {
+class Solution
+{
+    Node compute(Node head)
+    {
+        // your code here
+        
+        //1 Reverse LL
+        
+        head = rev(head);
+        
+        
+        //2.Leaders in an array
+        int maxVal = head.data;
+        Node curr = head.next, prev= head;
+        while(curr !=null ){
+            if(curr.data >=maxVal){
+                prev=curr;
+                maxVal = curr.data;
+            }
+            // curr.data < maxVal
+            else{
+                prev.next = curr.next;
+            }
+            curr =curr.next;
+        }
+        return rev(head);
+        
+    }
     
+    Node rev(Node head){
+        Node curr=head , prev=null, temp=null;
+        while(curr !=null){
+            temp =curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            
+        }
+        return prev;
+    }
 }
